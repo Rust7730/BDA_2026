7b1acb9f3aea63920f8036710cf5cb9968cf360b
Rustless <rudy_curioso@outlook.com>
2026-02-13 20:14:24 -0600

    nuevo verify

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 echo "========================================="
 echo "Verificando Vistas de la Base de Datos..."
 echo "========================================="
 
-# Listar las vistas
-docker exec -it store_postgres psql -U store_app -d store_db -c "\dv"
+
+docker exec -it store_postgres sh -c 'PGPASSWORD=$DB_APP_PASSWORD psql -U "$DB_APP_USER" -d "$POSTGRES_DB" -c "\dv"'
 
 echo "========================================="
 echo "Probando lectura de las vistas (Top 1 de cada una)..."
 
-docker exec -it store_postgres psql -U store_app -d store_db -c "SELECT * FROM vw_Topn_prodcuts LIMIT 1;"
-docker exec -it store_postgres psql -U store_app -d store_db -c "SELECT * FROM vw_unsold_products LIMIT 1;"
-docker exec -it store_postgres psql -U store_app -d store_db -c "SELECT * FROM vw_Greater_Buyers LIMIT 1;"
-docker exec -it store_postgres psql -U store_app -d store_db -c "SELECT * FROM vw_Stock_Categories LIMIT 1;"
-docker exec -it store_postgres psql -U store_app -d store_db -c "SELECT * FROM vw_participacion_producto LIMIT 1;"
+docker exec -it store_postgres sh -c 'PGPASSWORD=$DB_APP_PASSWORD psql -U "$DB_APP_USER" -d "$POSTGRES_DB" -c "SELECT * FROM vw_Topn_prodcuts LIMIT 1;"'
+docker exec -it store_postgres sh -c 'PGPASSWORD=$DB_APP_PASSWORD psql -U "$DB_APP_USER" -d "$POSTGRES_DB" -c "SELECT * FROM vw_unsold_products LIMIT 1;"'
+docker exec -it store_postgres sh -c 'PGPASSWORD=$DB_APP_PASSWORD psql -U "$DB_APP_USER" -d "$POSTGRES_DB" -c "SELECT * FROM vw_Greater_Buyers LIMIT 1;"'
+docker exec -it store_postgres sh -c 'PGPASSWORD=$DB_APP_PASSWORD psql -U "$DB_APP_USER" -d "$POSTGRES_DB" -c "SELECT * FROM vw_Stock_Categories LIMIT 1;"'
+docker exec -it store_postgres sh -c 'PGPASSWORD=$DB_APP_PASSWORD psql -U "$DB_APP_USER" -d "$POSTGRES_DB" -c "SELECT * FROM vw_participacion_producto LIMIT 1;"'
 
 echo "¡Verificación Completada!"
